ec9b474f54dde503cbf6b18e6e6886fe81024eaf
johnn <ikg1366@ya.ru>
2025-01-23 12:50:09 +0300

    payment_component

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { PromoModule } from '../promo/promo.module';
+import { ReferralModule } from '../referral/referral.module';
 
 @Module({
   providers: [PaymentService],
-  controllers: [PaymentController]
+  controllers: [PaymentController],
+  imports: [PrismaModule, PromoModule, ReferralModule],
+  exports: [PaymentService],
 })
 export class PaymentModule {}
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { ReferralService } from '../referral/referral.service';
+import { ICreatePayment, YooCheckout } from '@a2seven/yoo-checkout';
+import * as process from 'node:process';
+import { PromoService } from '../promo/promo.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  private checkout: YooCheckout;
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly ReferralService: ReferralService,
+    private readonly PromoService: PromoService,
+  ) {
+    this.checkout = new YooCheckout({ shopId: process.env.KASSA_SHOP_ID, secretKey: process.env.KASSA_SECRET_KEY });
+  }
+  async createPayment(): Promise<void> {
+    const idempotenceKey = uuidv4();
+
+    const createPayload: ICreatePayment = {
+      amount: {
+        value: '2.00',
+        currency: 'RUB',
+      },
+      payment_method_data: {
+        type: 'bank_card',
+      },
+      description: '',
+      confirmation: {
+        type: 'redirect',
+        return_url: 'https://yourwebsite.com/return-url',
+      },
+      metadata: {
+        user_id: '',
+        payment_id: '',
+      },
+    };
+
+    try {
+      const payment = await this.checkout.createPayment(createPayload, idempotenceKey);
+      console.log(payment);
+      // Здесь можно сохранять информацию о платеже в базу данных, если это необходимо
+    } catch (error) {
+      console.error(error);
+      // Обработка ошибки
+    }
+  }
+}
